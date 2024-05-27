6301b4be8c22a2e25a38775acea55dd6ca945c9d
Bismakhan23 <bismafarhan576@gmail.com>
2024-05-27 06:13:41 +0500

    first commit

--- assignment2/ch 6 to 9/app.js
+++ assignment2/ch 6 to 9/app.js
@@ -3,4 +3,11 @@ var num1=(10)
 var num2=(1)
 document.write(num1+num2)
 //question no 2
-var num3
+var a = 2
+var b = 3
+var result = (--a - --b + ++b + b--)
+document.write(result)
+//question no 3
+var useRname =prompt("Enter your name")
+//question no 3
+var userNumber=prompt("Enter your number")
---
+++ assignment3/chap9to13/app.js
@@ -0,0 +1,63 @@
+//question no 2
+
+var int1=prompt("Enter your int1")
+var int2=prompt("Enter your int2")
+
+if(int1>int2){
+document.write (int1 +"is larger than"+ int2);
+}
+else if(int1< int2){
+document.write(int2 + "is less than" +int1)
+}
+else{
+    document.write("Both integers are equal.");
+}
+//question no 3
+var userNUm=prompt("Enter your number")
+
+if(userNUm > 0){
+    document.write("Positive number")
+}
+else if(userNUm == 0){
+    document.write("Negative number")
+}
+//question no 4
+var voWel=prompt("Enter your alphabet")
+
+if(voWel===i){
+ document.write("True")
+}
+else{
+    document.write("false")
+}
+//question no 5 
+var passwoRD=("Fraday's law")
+var userPAssword=prompt("Enter your password")
+if(userPAssword==passwoRD){
+    document.write("correct")
+}
+else{
+    document.write("Incorrect password")
+}
+//question no 6
+var houR=(13)
+if(hour<18){
+document.write("Good Morning")
+}
+else {
+    document.write("Good bye")
+}
+//question no 7
+var nuM=("24 Hour's")
+var nuM2=(1900)
+var useRNum=prompt("Enter your num")
+
+if (useRNum==nuM2){
+ document.write("True")
+}
+else if{
+      document.write(useRNum)
+}
+else{
+    document.write(Bye)
+}

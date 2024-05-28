d82d671557fa5ee2276e7993b499e94a3e3640c3
Bismakhan23 <bismafarhan576@gmail.com>
2024-05-29 02:26:39 +0500

    first commit

---
+++ assignment5/chapter 14 to 16/app.js
@@ -0,0 +1,8 @@
+//question no 3
+var mixedARRay=["a","b","c","d"]
+console.log(mixedARRay)
+//question no 4
+var miXEdarray=["1","2","3","4"]
+console.log(miXEdarray)
+//question no 5
+var mixeDarray=[""]
